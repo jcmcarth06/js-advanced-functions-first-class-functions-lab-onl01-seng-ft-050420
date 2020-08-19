d40005803d03ef303a5f693fea50bf9f70d20378
Joshua McArthur <jcmcarth06@gmail.com>
2020-08-19 20:20:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ const returnLastTwoDrivers = function(drivers) {
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
 function createFareMultiplier(multiplier) {
-  return function(fare)
-}
+  return function(fare) {
+    return fare * multiplier;
+  }
+};
